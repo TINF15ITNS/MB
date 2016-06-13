ab09e46c8a3b10decbdea851120f8afe41c9c101
NikolaiGit <bubiseip@web.de>
2016-06-13 15:21:49 +0200

    Der Fehler, dass die actionPerformed()-Methode nicht aufgerufen wurde, ist behoben worden

--- src/Producer/GUIProducerErzeugen.java
+++ src/Producer/GUIProducerErzeugen.java
@@ -4,41 +4,40 @@ import java.awt.BorderLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class GUIProducerErzeugen extends JFrame implements ComponentListener, ActionListener {
+public class GUIProducerErzeugen extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	JButton erzeugeProducerButton;
 	ProducerErzeugen producerErzeugen;
 
-	public static void main(String[] args) {
-		GUIProducerErzeugen gui = new GUIProducerErzeugen(new ProducerErzeugen());
-	}
+	/*
+	 * To Do:
+	 * 
+	 * Vielleicht die Sache mit ComponentListener und ActionListener mttels innerer oder anonoymer Klassen implmentieren
+	 */
 
 	public GUIProducerErzeugen(ProducerErzeugen p) {
 		producerErzeugen = p;
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(250, 75);
+		this.setSize(400, 75);
 		this.setTitle("Erzeugen von Produzenten");
 		// x-Position: in Mitte der linken Fensterhälfte
 		// y-Position: bei 0.05 der Bildschirmgröße
 		this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width / 4) - (this.getWidth() / 2),
 				(int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.05));
 		this.setLayout(new BorderLayout());
-		this.addComponentListener(this);
 
 		erzeugeProducerButton = new JButton("Erzeuge einen neuen Produzenten");
 		this.add(erzeugeProducerButton, BorderLayout.CENTER);
+		erzeugeProducerButton.addActionListener(this);
 		erzeugeProducerButton.setSize(220, 70);
 
 		this.setVisible(true);
 	}
 
-	// Springt nicht hierrein
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if (arg0.equals(erzeugeProducerButton)) {
@@ -46,29 +45,4 @@ public class GUIProducerErzeugen extends JFrame implements ComponentListener, Ac
 		}
 
 	}
-
-	@Override
-	public void componentHidden(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void componentMoved(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void componentResized(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void componentShown(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
