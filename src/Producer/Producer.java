4c11b3803ffffe880d0b8a6a3f9657ba969e643b
NikolaiGit <bubiseip@web.de>
2016-06-27 13:00:39 +0200

    Hier hatte sich was vergessen bei Producer zu löschen, als ich die Klasse Key entfernt habe

--- src/Producer/Producer.java
+++ src/Producer/Producer.java
@@ -1,19 +1,10 @@
 package Producer;
 
-import MessageServer.Key;
-
 public class Producer {
 	String name;
-	Key key;
 
 	public Producer(String n) {
-		System.out.println("Bin im Konstruktor von Producer");
 		this.name = n;
-	}
-
-	public boolean setKey(Key k) {
-		key = k;
-		return true;
 	}
 
 }
