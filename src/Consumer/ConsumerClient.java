cfba2e91e36bcf7b7ccdd340b3ebd8ce5248dada
Sebastian <Sebastian@DESKTOP-3GC9GPH>
2016-07-02 14:20:47 +0200

    Changes the testConnection method to check via TCP

--- src/Consumer/ConsumerClient.java
+++ src/Consumer/ConsumerClient.java
@@ -5,42 +5,48 @@ import java.net.*;
 import java.util.Scanner;
 
 public class ConsumerClient {
-	private static int serverPort = 55555;
-	
+	private static int serverPort = 55555; //The address of the MessageServer
+
 	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Um Nachrichten erhalten zu können, muss dieser Client mit dem Server kommunizieren können...");
 		boolean correctInetAddress = false;
-		String address = null;		
-
-		do {
-			System.out.print("Geben Sie bitte die Adresse des Servers an: ");
-			address = scanner.nextLine();
-			if (!correctInetAddress)
-				correctInetAddress = testConnection(address);
-		} while (!correctInetAddress);
-
-		// Erzeuge ein Consumer
-		Consumer user = new Consumer(InetAddress.getByName(address), serverPort);
+		InetAddress address = null;
+		Scanner scanner = new Scanner(System.in);
+		
+		//Ask for a new address until a reachable one is provided
+		while (!correctInetAddress) {
+			System.out.print("Geben Sie bitte die Adresse des Servers (an ohne Port): ");
+			try {
+				address = InetAddress.getByName(scanner.nextLine()); //Parse the given string into a InetAddress object
+				correctInetAddress = testConnection(address); //Test if there is someone listening via TCP
+				
+			} catch (UnknownHostException e) {
+				System.out.println(e.getMessage());
+				continue;
+			}
+		}
+		
+		Consumer user = new Consumer(address, serverPort);
 		user.registerOnServer();
 		user.registerOnMulticastGroup();
 		user.startAction();
+		
 
 	}
 
-	private static boolean testConnection(String adress) 
-	{	
-			try {
-				if (!InetAddress.getByName(adress).isReachable(50)) {
-					System.out.println("Der Server ist unter der Adresse: " + adress.toString() + " nicht erreichbar");
-					return false;
-				}
-			} catch (Exception e) {
-				System.out.println("Der Server ist unter der Adresse: " + adress.toString() + " nicht erreichbar");
-				return false;
-			}
-		return true;
+	/**
+	 * Checks if it is possible to establish a TCP connection using the "serverPort"
+	 * @param adress The address of the server to be checked
+	 * @return if the connection was successful 
+	 */
+	private static boolean testConnection(InetAddress adress) {
+		Socket server = new Socket();
+		try {
+			server.connect(new InetSocketAddress(adress, serverPort), 1000);
+			server.close();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+
 	}
 }
