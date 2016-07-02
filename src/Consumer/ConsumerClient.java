837e3d6b52f2ff10ffd248df756a98d4472b6cce
Sebastian <Sebastian@DESKTOP-3GC9GPH>
2016-07-02 12:25:42 +0200

    Fixed ConsumerClient

--- src/Consumer/ConsumerClient.java
+++ src/Consumer/ConsumerClient.java
@@ -5,43 +5,42 @@ import java.net.*;
 import java.util.Scanner;
 
 public class ConsumerClient {
-
+	private static int serverPort = 55555;
+	
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Um Nachrichten erhalten zu können, muss dieser Client mit dem Server kommunizieren können...");
 		boolean correctInetAddress = false;
-		InetAddress iadr = null;
-		int portServer = 0;
+		String address = null;		
 
 		do {
 			System.out.print("Geben Sie bitte die Adresse des Servers an: ");
-			iadr = InetAddress.getByName(scanner.nextLine());
-			if (!correctInetAddress) correctInetAddress = testConnection(iadr);
+			address = scanner.nextLine();
+			if (!correctInetAddress)
+				correctInetAddress = testConnection(address);
 		} while (!correctInetAddress);
 
 		// Erzeuge ein Consumer
-		Consumer prod = new Consumer(name, iadr, 50555);
-		prod.registerOnServer();
-		prod.registerOnMulticastGroup();
-		prod.registerOnProducers();
-		// mit dieser Methode startet man die Möglichkeit für den Anwender, iwas
-		// zu machen ...
-		prod.startAction();
+		Consumer user = new Consumer(InetAddress.getByName(address), serverPort);
+		user.registerOnServer();
+		user.registerOnMulticastGroup();
+		user.startAction();
 
 	}
 
-	private static boolean testConnection(InetAddress adress) 
+	private static boolean testConnection(String adress) 
 	{	
 			try {
-				if (!adress.isReachable(50)) {
+				if (!InetAddress.getByName(adress).isReachable(50)) {
 					System.out.println("Der Server ist unter der Adresse: " + adress.toString() + " nicht erreichbar");
 					return false;
 				}
-			} catch {
+			} catch (Exception e) {
 				System.out.println("Der Server ist unter der Adresse: " + adress.toString() + " nicht erreichbar");
 				return false;
 			}
 		return true;
-}}
+	}
+}
