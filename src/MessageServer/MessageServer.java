a9a86866ef10e2564041610e81a41df40bcfd8e8
NikolaiGit <bubiseip@web.de>
2016-07-02 17:06:56 +0200

    defined the MessageServer-structure

--- src/MessageServer/MessageServer.java
+++ src/MessageServer/MessageServer.java
@@ -1,7 +1,14 @@
 package MessageServer;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import Message.Message;
 
 public class MessageServer {
 
@@ -12,27 +19,17 @@ public class MessageServer {
 	HashMap<Integer, Customer> hashMapProducer, hashMapConsumer;
 	Scanner scanner;
 
-	public static void main(String[] args) {
-		MessageServer messageServer = new MessageServer();
-
-	}
-
-	public MessageServer() {
+	public MessageServer(int pms) {
 		hashMapProducer = new HashMap<>();
 		hashMapConsumer = new HashMap<>();
+		portMessageServer = pms;
 
-		scanner = new Scanner(System.in);
-		System.out.println("Willkommen beim Installationsvorgang Ihres MessageServers:");
-		System.out.print("Geben Sie bitte den Port an, über welchen Konsumenten und Produzenten den MessageServer erreichen können ");
-		portMessageServer = scanner.nextInt();
-
-		System.out.println("\n\n\nIhr MessageServer wurde erfolgreich installiert\nSie erreichen den MessageServer unter folgendem Port: " + portMessageServer);
 	}
 
 	// Oder das hier in der MainMethode in eigene threads auslagern
 	// da hab ich mir noch keine gedanken drüber gemacht
 	/**
-	 * waits for Messages
+	 * waits for Messages from Producers or Consumers
 	 */
 	public void getMessages() {
 		// diese Methode hält den einen Port offen, wo auf nachrichten der konsumenten und Produzenten gehört wird
@@ -40,34 +37,186 @@ public class MessageServer {
 
 		// hier ist dann nen ServerSocket, dass nen neuen thread startet, wenn eine Anfrage kommt.
 		// wie Skript Folie 37 E
-	}
 
-	/**
-	 * In this Method the MessageServer offers the available Producers
-	 */
-	public void offerProducer() {
-
-	}
-
-	/**
-	 * This Method accepts the register-Messages and saves the Producer and Consuments.
-	 */
-	public void register() {
-		// kommt ne registrierungsanfrage, dann wird hier ne id erzeugt und als Message zurückgesendet
-		// und es wird ein ConsumerMS Objekt erzeugt ( siehe Interface Customer) und in der HashMap gespeichert
-		// Key Value ist die id
-	}
-
-	/**
-	 * this method forwards the messages of the Producers to the Consumers
-	 */
-	public void recievePushMessage() {
+		try (ServerSocket serverSo = new ServerSocket(4711)) {
+			Socket clientSo = null;
+			while (true) {
+				clientSo = serverSo.accept();
+				Thread t = new Thread(new MessageHandler(clientSo));
+				t.start();
+			}
+		} catch (IOException e) {
+			System.out.println("IOFehler beim ServerSocket");
+			e.printStackTrace();
+		}
 
 	}
 
-	/**
-	 * 
-	 */
-	public void deregister() {
+	class MessageHandler implements Runnable {
+		Socket client;
+
+		public MessageHandler(Socket client) {
+			this.client = client;
+		}
+
+		@Override
+		public void run() {
+			try {
+				ObjectInputStream in = new ObjectInputStream(client.getInputStream());
+				Message m = (Message) in.readObject();
+
+				/*
+				 * RegisterConsumer, DeregisterConsumer, SubscribeProducers, UnsubscribeProducers, getProducerList, getSubscriptions, Message, RegisterProducer,
+				 * DeregisterProducer,
+				 */
+				Message answer = null;
+				switch (m.getType()) {
+				case DeregisterConsumer:
+					answer = deregisterConsumer(m);
+					break;
+				case DeregisterProducer:
+					answer = deregisterProducer(m);
+					break;
+				case Message:
+					answer = recieveMessageFromProducer(m);
+					break;
+				case RegisterConsumer:
+					answer = registerConsumer(m);
+					break;
+				case RegisterProducer:
+					answer = registerProducer(m);
+					break;
+				case SubscribeProducers:
+					answer = subscribeProducers(m);
+					break;
+				case UnsubscribeProducers:
+					answer = unsubscribeProducers(m);
+					break;
+				case getProducerList:
+					answer = getProducerList(m);
+					break;
+				case getSubscriptions:
+					answer = getSubscriptions(m);
+					break;
+				default:
+					throw new RuntimeException("Dieser Fall kann nicht eintreten");
+
+				}
+
+				ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+				out.writeObject(answer);
+
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		/**
+		 * 
+		 * @param m
+		 * @return
+		 */
+		public Message getSubscriptions(Message m) {
+			return null;
+		}
+
+		/**
+		 * In this Method the MessageServer offers the available Producers
+		 * 
+		 * @param m
+		 * @return
+		 */
+		private Message getProducerList(Message m) {
+			// heißt er generiert ne Antwort-Message und returned diese
+			return null;
+
+		}
+
+		/**
+		 * This Method registers the consumers.
+		 * 
+		 * @return
+		 */
+		private Message registerConsumer(Message m) {
+			// kommt ne registrierungsanfrage, dann wird hier ne id erzeugt und als Message zurückgesendet
+			// und es wird ein ConsumerMS Objekt erzeugt ( siehe Interface Customer) und in der HashMap gespeichert
+			// Key Value ist die id
+			// und erzeugte id und die MulticastAdresse werden im neuen Message-Objekt gespeichert und zurückgegeben.
+			return null;
+		}
+
+		/**
+		 * This Method accepts the register-messages and saves the producer.
+		 * 
+		 * @return
+		 */
+		private Message registerProducer(Message m) {
+			// kommt ne registrierungsanfrage, dann wird hier ne id erzeugt und als Message zurückgesendet
+			// und es wird ein ConsumerMS Objekt erzeugt ( siehe Interface Customer) und in der HashMap gespeichert
+			// Key Value ist die id
+			// und erzeugte id und die MulticastAdresse werden im neuen Message-Objekt gespeichert und zurückgegeben.
+			return null;
+		}
+
+		/**
+		 * this method forwards the messages of the Producers to the Consumers
+		 * 
+		 * @param m
+		 * @return
+		 */
+		private Message recieveMessageFromProducer(Message m) {
+			// die große Frage: gibt es eine MulticastGruppe oder emhrere?
+			return null;
+
+		}
+
+		/**
+		 * unsubscribes the Consumer, who sent the message, on the server
+		 * 
+		 * @param m
+		 *            sended message
+		 * @return answer-message
+		 */
+		private Message deregisterConsumer(Message m) {
+			return null;
+		}
+
+		/**
+		 * unsubscribes the producer, who sent the message, on the server
+		 * 
+		 * @param m
+		 *            sended message
+		 * @return answer-message
+		 */
+		private Message deregisterProducer(Message m) {
+			return null;
+		}
+
+		/**
+		 * subscribes the consumer, who sent the message, at the desired producers
+		 * 
+		 * @param m
+		 * @return
+		 */
+		private Message subscribeProducers(Message m) {
+			return null;
+
+		}
+
+		/**
+		 * unsubscribes the consumer, who sent the message, at the desired producers
+		 * 
+		 * @param m
+		 * @return
+		 */
+		private Message unsubscribeProducers(Message m) {
+			return null;
+
+		}
 	}
 }
