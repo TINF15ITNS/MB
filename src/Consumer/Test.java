abdf8411c6dc51535c93a25f2b4ad084451e440b
oiooio <basti.mattheis@gmail.com>
2016-07-02 14:42:12 +0200

    Made Testclass more readable

--- src/Consumer/Test.java
+++ src/Consumer/Test.java
@@ -13,68 +13,29 @@ import Message.PayloadRegisterOnServer;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ServerSocket ss = null;
 		ObjectOutputStream out = null;
 		ObjectInputStream in = null;
 		Socket client = null;
-		try {
-			ss = new ServerSocket(55555);
-			while (true) {
-				System.out.println("Warte auf Anfrage eines Clients");
-				client = ss.accept();
-				System.out.println("Anfrage akzeptiert");
 
-				out = new ObjectOutputStream(client.getOutputStream());
-				in = new ObjectInputStream(client.getInputStream());
+		ss = new ServerSocket(55555);
+		while (true) {
+			System.out.println("Warte auf Anfrage eines Clients");
+			client = ss.accept();
+			System.out.println("Anfrage akzeptiert");
 
-				System.out.println("Habe Streams geöffnet");
+			out = new ObjectOutputStream(client.getOutputStream());
+			in = new ObjectInputStream(client.getInputStream());
 
-				System.out.println("Lese jetzt die Nachricht");
-				Message m = (Message) in.readObject();
-				Message answer = new Message(MessageType.RegisterOnServer, new PayloadRegisterOnServer(1234, InetAddress.getByName("228.5.6.7")));
-				System.out.println("Schreibe jetzt die nachricht");
-				out.writeObject(answer);
-			}
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException");
-		} catch (IOException e) {
-			System.out.println("IOFehler");
-			e.printStackTrace();
-		} finally {
+			System.out.println("Habe Streams geöffnet");
 
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (client != null) {
-				try {
-					client.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (ss != null) {
-				try {
-					ss.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			System.out.println("Lese jetzt die Nachricht");
+			Message m = (Message) in.readObject();
+			Message answer = new Message(MessageType.RegisterOnServer,
+					new PayloadRegisterOnServer(1234, InetAddress.getByName("228.5.6.7")));
+			System.out.println("Schreibe jetzt die nachricht");
+			out.writeObject(answer);
 		}
 
 	}
