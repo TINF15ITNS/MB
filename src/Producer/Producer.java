609cc21ea2dbd4eb4bb3641ef95251a152e7dfdc
Fabian Hinz <hallo@naibafo.de>
2016-07-03 14:42:22 +0200

    implement producer Constructor and helping functions
    Mostly the same as in Consumer.
    Maybe move to an utility class

--- src/Producer/Producer.java
+++ src/Producer/Producer.java
@@ -1,10 +1,55 @@
 package Producer;
 
-public class Producer {
-	String name;
+import java.io.*;
+import java.net.*;
+import Message.*;
 
-	public Producer(String n) {
-		this.name = n;
+public class Producer {
+	private static int serverPort = 55555;
+	private String name;
+	private InetAddress serverAddress;
+
+	public Producer(String name, String address) throws IOException{
+		this.serverAddress = InetAddress.getByName(address);
+		if (!testConnection(serverAddress, 1000))
+			throw new IOException("There is no server at the given address");
+		this.name = name;
 	}
+	private boolean testConnection(InetAddress address, int timeout) {
+		Socket server = new Socket();
+		try {
+			server.connect(new InetSocketAddress(address, serverPort), timeout);
+			server.close();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	public String[] getProducers() {
+		Message answer = this.sendAndGetMessage(new Message(MessageType.getProducerList, null), serverAddress);
+		return ((PayloadGetProducerList) answer.getPayload()).getProducers();
+	}
+	private Message sendAndGetMessage(Message message, InetAddress address) {
+		Socket server;
+		try {
+			server = new Socket(address, serverPort);
+
+			Message answer = null;
+			ObjectOutputStream out = null;
+			ObjectInputStream in = null;
+
+			out = new ObjectOutputStream(server.getOutputStream());
+			in = new ObjectInputStream(server.getInputStream());
+			out.writeObject(message);
+			answer = (Message) in.readObject();
+
+			in.close();
+			out.close();
+			server.close();
+			return answer;
+		} catch (Exception e) {
+			return null;
+		}
+}
 
 }
