8b7dd1236455209ecd4e17fce47db186a6b94cc2
Fabian Hinz <hallo@naibafo.de>
2016-07-03 15:06:44 +0200

    Implement payloadDeregisterProducer

--- src/message/PayloadDeregisterProducer.java
+++ src/message/PayloadDeregisterProducer.java
@@ -1,12 +1,16 @@
-/**
- * 
- */
 package message;
 
-/**
- * @author 
- *
- */
+
 public class PayloadDeregisterProducer implements Payload {
+	
+	private final String name;
+	
+	public PayloadDeregisterProducer(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 
 }
