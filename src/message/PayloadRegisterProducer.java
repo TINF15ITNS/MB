a1ab73a7d9e9be1bd29fdae710d67838286b3c42
Fabian Hinz <hallo@naibafo.de>
2016-07-03 15:11:23 +0200

    implement payloadRegisterProducer

--- src/message/PayloadRegisterProducer.java
+++ src/message/PayloadRegisterProducer.java
@@ -1,5 +1,15 @@
 package message;
 
 public class PayloadRegisterProducer implements Payload {
+	
+	private final String name;
+	
+	public PayloadRegisterProducer(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 
 }
