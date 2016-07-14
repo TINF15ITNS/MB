079d31ec53aa3ad30eb8ec8d5d340b6962132608
NikolaiGit <bubiseip@web.de>
2016-07-14 19:11:04 +0200

    missed to change the attribute-type in ProducerCLI

--- src/producer/ProducerCLI.java
+++ src/producer/ProducerCLI.java
@@ -7,7 +7,7 @@ public class ProducerCLI {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Producer prod = null;
+		ProducerIF prod = null;
 		boolean exit = false;
 
 		System.out.println("Herzlich Willkommen\n\n");
