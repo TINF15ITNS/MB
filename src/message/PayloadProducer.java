d60c6c4b5d0bf9e34c0c0f36dfddd910b2e46058
NikolaiGit <bubiseip@web.de>
2016-07-22 23:00:03 +0200

    added a comment in PayloadProducer

--- src/message/PayloadProducer.java
+++ src/message/PayloadProducer.java
@@ -4,25 +4,37 @@ import java.io.Serializable;
 
 public class PayloadProducer implements Payload, Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * name of the producer, who wants to register or deregister
+	 */
 	private final String name;
+
 	private boolean success = false;
 
 	public PayloadProducer(String name) {
 		this.name = name;
 	}
+
 	public PayloadProducer(String name, boolean success) {
 		this.name = name;
 		this.success = success;
 	}
+
 	public PayloadProducer(boolean success) {
 		this.name = null;
 		this.success = success;
 	}
-	
+
+	/**
+	 * 
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
+
 	@Override
 	public boolean getSuccess() {
 		return success;
